d1f0edf9c8ef16ba12b26766c1989f1369afe5bf
ThatTonybo <twatt74@eq.edu.au>
2019-10-10 17:56:41 +1000

    Make sure variables are named

--- functions/update.py
+++ functions/update.py
@@ -2,14 +2,14 @@ import pyglet
 
 def keys(pressed, player):
     if pressed['W'] == True:
-        player.up(0.5)
+        player.up(by=0.5)
 
     if pressed['A'] == True:
-        player.left(0.5)
+        player.left(by=0.5)
 
     if pressed['S'] == True:
-        player.down(0.5)
+        player.down(by=0.5)
 
     if pressed['D'] == True:
-        player.right(0.5)
+        player.right(by=0.5)
 
